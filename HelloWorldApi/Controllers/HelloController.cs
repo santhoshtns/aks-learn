4ab1123f15d89dc6ac7b2121c6d30abc7c98d16a
Santhosh Sampath <santhosh.sampath@sea.live>
2024-10-12 23:41:53 +0800

    sample docker image

---
+++ HelloWorldApi/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World from .NET 8 Web API!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # aks-learn
+
+Steps to Run docker image
+
+1. Docker build
+docker build -t helloworldapi .
+
+1. Run Docker image
+docker run -d -p 8889:80 helloworldapi
